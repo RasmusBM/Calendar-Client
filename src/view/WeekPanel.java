d40767bc8daa234e9a9d29eee1b14320f44660e4
RasmusBM <rasmus_bm@hotmail.com>
2014-11-27 10:57:25 +0100

    Added new DisplayDate method

--- src/view/WeekPanel.java
+++ src/view/WeekPanel.java
@@ -36,6 +36,7 @@ public class WeekPanel extends JPanel{
 	private JPanel p3;
 	private static int START_WEEK;
 	private static int START_YEAR;
+	private static String MONTHDAYSEPARATOR =" ";
 	
 	//Declaration of panel constants
 	public static final String PREVIOUS = "Previous";
@@ -53,7 +54,7 @@ public class WeekPanel extends JPanel{
 		START_WEEK = cal.get(GregorianCalendar.WEEK_OF_YEAR);
 		START_YEAR = cal.get(GregorianCalendar.YEAR);
 		
-//		setLayout(new GridLayout(7,7));
+		
 		setLayout(new BorderLayout());
         p1 = new JPanel(new GridLayout(0,7));
         p1.setBackground(Color.red);
@@ -82,7 +83,8 @@ public class WeekPanel extends JPanel{
             
         }
         
-        displayDate(START_WEEK,START_YEAR);
+//        displayDate(START_WEEK,START_YEAR);
+        displayDate2(START_WEEK,START_YEAR);
 
         p3 = new JPanel(new GridLayout(1,5));
         p3.setBackground(Color.green);
@@ -178,15 +180,51 @@ public class WeekPanel extends JPanel{
         }
     }
     
+    public void displayDate2(int weekNumber, int yearNumber){
+    	
+    	cal.setFirstDayOfWeek(Calendar.MONDAY);
+    	cal.set(Calendar.WEEK_OF_YEAR, START_WEEK);
+    	cal.set(Calendar.YEAR, START_YEAR);
+    	
+    	
+    	int weekday = cal.get(Calendar.DAY_OF_WEEK);
+    	cal.add(Calendar.DATE,1 - weekday);
+    	int iMonth, iDay;
+    	
+    	for (int x = 0; x <  button.length; x++){
+    		iMonth =  cal.get(Calendar.MONTH);
+    		iDay = cal.get(Calendar.DAY_OF_MONTH);
+    		switch(iMonth){
+    		case 0: button[x].setText("Jan" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 1:button[x].setText("Feb" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 2:button[x].setText("Mar" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 3:button[x].setText("Apr" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 4:button[x].setText("Maj" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 5:button[x].setText("Jun" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 6:button[x].setText("Jul" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 7:button[x].setText("Aug" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 8:button[x].setText("Sep" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 9:button[x].setText("Okt" + MONTHDAYSEPARATOR + (iDay) );break;
+    		case 10:button[x].setText("Nov" + MONTHDAYSEPARATOR + (iDay) );;break;
+    		case 11:button[x].setText("Dec" + MONTHDAYSEPARATOR + (iDay) );break;
+    		}
+    		cal.add(Calendar.DATE, 1);
+    	}
+    	
+    }
+
+    
     public void refreshDate(int newWeek){
     	
-//    	int hej = START_WEEK += newWeek;
-//    	String hej2 = String.valueOf(hej);
+    	int nextWeek = START_WEEK += newWeek;
     	
-    	displayDate(START_WEEK += newWeek,START_YEAR);
+    	String stringNextWeek = String.valueOf(nextWeek);
     	
-//    	uge.setText(hej2);
+    	displayDate2(START_WEEK += newWeek,START_YEAR);
+    	
+    	uge.setText(stringNextWeek);
     
+    	nextWeek = START_WEEK -= newWeek;
     	
     }
     
